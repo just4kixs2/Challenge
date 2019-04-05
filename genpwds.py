5ec8da748c901ed9246d907d2ebb7cc628528f3a
just4kixs2 <49287898+just4kixs2@users.noreply.github.com>
2019-04-05 09:16:25 -0400

    Updated comment lines

--- genpwds.py
+++ genpwds.py
@@ -12,11 +12,11 @@ file = open(filename,'w')	#open file for writing
 #define function that create random ASCII letters/digits
 def randomString(stringLength = PasswordLength):
     letters = string.ascii_letters
-    numbers = string.digits
-    return ''.join(random.choice(letters + numbers) for i in range(stringLength))
+    digits = string.digits
+    return ''.join(random.choice(letters + digits) for i in range(stringLength))
 
 for i in range(1,NumberPasswords):
-	file.write(randomString()+'\n')		#call randomString and write it to file 100x
+	file.write(randomString()+'\n')		#call randomString and write it to passwords file
 	i += 1
 
 file.close()
