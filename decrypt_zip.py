752330aa26f552bb970bd589912ed4068662b70f
just4kixs2 <49287898+just4kixs2@users.noreply.github.com>
2019-04-05 16:47:35 -0400

    Decrypts only zip and 7zip archives

---
+++ decrypt_zip.py
@@ -0,0 +1,26 @@
+from zipfile import ZipFile
+import time
+
+NumberPasswords = 100
+
+filename = 'passwords/passwords'+str(NumberPasswords)+'.txt'	#define password list file
+file = open(filename,'r')	#open file for reading
+
+passwords = [line.rstrip('\n') for line in file] # read line by line and strip newline characters
+
+starttime = time.time()
+
+for i in passwords:
+	zip_archive = ZipFile('archive1a.zip')
+	try:
+		zip_archive.extractall(pwd=i)
+	except:
+		pass
+	zip_archive = ZipFile('archive2.7z')
+	try:
+		zip_archive.extractall(pwd=i)
+	except:
+		pass
+
+print(time.time() - starttime)	
+file.close()
